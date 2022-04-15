95f0f4637efaac1be151f3bbe3380190504e4b37
Max <pogatop@yandex.ru>
2022-04-15 19:19:22 +0300

    added user dto

---
+++ User/DataTransferObjects.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Orders.User;
+
+public class RegisterUserDto
+{
+    /// <summary>User display name</summary>
+    /// <example>Jonh Doe</example>
+    [Required]
+    [MaxLength(50)]
+    [RegularExpression(@"^[a-zA-Zа-ЯА-Я\s]*$")]
+    public string DisplayName { get; set; }
+    
+    /// <summary>Username for authorization</summary>
+    /// <example>jonh_doe333</example>
+    [Required]
+    [MaxLength(30)]
+    [RegularExpression(@"^[\w\s\d]*$")]
+    public string Username { get; set; }
+    
+    /// <summary>Password for authorization</summary>
+    /// <example>pass333</example>
+    [Required]
+    [MinLength(6)]
+    [MaxLength(20)]
+    [RegularExpression(@"^[\w\s\d]*$")]
+    public string Password { get; set; }
+}
+
+public class ReadUserDto
+{
+    /// <summary>User identifier</summary>
+    /// <example>xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</example>
+    public Guid Id { get; set; }
+    
+    /// <summary>User display name</summary>
+    /// <example>Jonh Doe</example>
+    public string DisplayName { get; set; }
+    
+    /// <summary>Username for authorization</summary>
+    /// <example>jonh_doe333</example>
+    public string Username { get; set; }
+}
