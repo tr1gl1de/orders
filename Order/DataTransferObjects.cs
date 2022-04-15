43850174f67250709a605992e3c6b3e64c13cbe6
Max <pogatop@yandex.ru>
2022-04-15 17:11:09 +0300

    added update order dto

--- Order/DataTransferObjects.cs
+++ Order/DataTransferObjects.cs
@@ -39,6 +39,43 @@ public class CreateOrderDto
     public double Weight { get; set; }
 }
 
+public class UpdateOrderDto
+{
+    /// <summary>Город отправителя</summary>
+    /// <example>Москва</example>
+    [Required]
+    [MaxLength(20)]
+    [RegularExpression(@"^[a-zA-Zа-яА-Я]*[\s\-]?[a-zA-Zа-яА-Я]*[\.\s]?$")]
+    public string CitySender { get; set; } = string.Empty;
+
+    /// <summary>Адресс отправителя</summary>
+    /// <example>Москва, ул. Пушкина , д.1, кв 23</example>
+    [Required]
+    [MaxLength(100)]
+    [RegularExpression(@"[a-zA-Zа-яА-Я\,0-9\-\s\.]*$")]
+    public string AddressSender { get; set; } = string.Empty;
+
+    /// <summary>Город получателя</summary>
+    /// <example>Подольск</example>
+    [Required]
+    [MaxLength(20)]
+    [RegularExpression(@"^[a-zA-Zа-яА-Я]*[\s\-]?[a-zA-Zа-яА-Я]*[\.\s]?$")]
+    public string CityReceiver { get; set; } = string.Empty;
+
+    /// <summary>Адресс получателя</summary>
+    /// <example>обл. Московская, г. Подольск, ул. А.Невского, д. 123-4, кв 45</example>
+    [Required]
+    [MaxLength(100)]
+    [RegularExpression(@"[a-zA-Zа-яА-Я\,0-9\-\s\.]*$")]
+    public string AddressReceiver { get; set; } = string.Empty;
+
+    /// <summary>Вес посылки (кг)</summary>
+    /// <example>2.5</example>
+    [Required]
+    [Range(0.001, 1000)]
+    public double Weight { get; set; }
+}
+
 public class ReadOrderDto
 {
     /// <summary>Идентификатор заказа</summary>
